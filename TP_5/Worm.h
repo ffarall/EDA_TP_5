103fb59ed14f53a2d30dc9413cddaecdb1496167
facundofarall <33322493+facundofarall@users.noreply.github.com>
2018-09-01 19:20:05 -0300

    actualizando

--- TP_5/Worm.h
+++ TP_5/Worm.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Vector.h"
+#include "Event.h"
 
 #define WORM_SPEED (27*50) // Worms move at 27 pixels per second, and frameCounter is 1/50 of a second (50 FPS)
 #define WORM_GRAVITY 0.24
 #define PI 3.14159
 
-typedef enum { START_MOVING, MOVING, STOP_MOVING, RESTING, JUMP } wormStates_n;
+typedef enum { START_MOVING, MOVING, STOP_MOVING, RESTING, JUMP } wormState_n;
+typedef enum { KEY_MOVE_RIGHT_DOWN, KEY_MOVE_LEFT_DOWN, KEY_MOVE_UP, NEW_FRAME, KEY_JUMP_DOWN } wormEvent_n;
 typedef unsigned int uint;
 
 class Worm
@@ -13,6 +15,8 @@ class Worm
 public:
 	Worm();
 	Worm(Vector pos_);
+	Worm(Vector pos_, char ku, char kl, char kr);
+	Worm(double x_, double y_, char ku, char kl, char kr);
 	Worm(double x_, double y_);
 	~Worm();
 
@@ -20,7 +24,7 @@ public:
 	bool get_orientation();
 	uint get_frameCounter();
 
-	void set_pos(Vector);
+	void set_pos(Vector pos_);
 	void set_pos(double x_, double y_);
 	void set_orientation(bool orient);
 	void set_frameCounter(uint fc);
@@ -28,12 +32,19 @@ public:
 	void update(Worm * wArray[], int wormCount);
 
 private:
-	wormStates_n currentState; // START_MOVING, MOVING, STOP_MOVING, RESTING, JUMP
+	wormState_n currentState;
 	Vector pos;
+	char keyUp;
+	char keyRight;
+	char keyLeft;
 	const double speed = WORM_SPEED;
 	const double g = WORM_GRAVITY;
 	const double angle = PI / 3.0;
 	bool lookingRight;
 	uint frameCounter;
+
+	uint jumpDuration;
+
+	wormEvent_n event_decoder(Event& ev);
 };
 
