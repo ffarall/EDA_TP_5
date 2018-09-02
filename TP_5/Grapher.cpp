9febd53891b4be026eeefd68ac2a43016087302d
Pablo Scheinfeld <scheinpablo@yahoo.com>
2018-09-01 19:58:39 -0700

    grapher update

--- TP_5/Grapher.cpp
+++ TP_5/Grapher.cpp
@@ -1,5 +1,6 @@
 #include "Grapher.h"
 #include <allegro5/allegro_image.h>
+#include <string> 
 
 Grapher::Grapher(int xDisplay, int yDisplay)
 {
@@ -21,6 +22,26 @@ void Grapher::destroy()
 	al_destroy_bitmap(background);
 }
 
+void Grapher::update(Worm * wArray[], int wormCount)
+{
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	
+	for (int i = 0; i < wormCount; i++)
+	{
+		if (wArray[i]->get_current_state() == MOVING)
+		{
+			al_draw_bitmap(walkingImages[wArray[i]->get_frameCounter()], wArray[i]->get_pos.get_x(), wArray[i]->get_pos.get_y(), 0);
+		}
+		else if (wArray[i]->get_current_state() == JUMP)
+		{
+			al_draw_bitmap(jumpingImages[wArray[i]->get_frameCounter()], wArray[i]->get_pos.get_x(), wArray[i]->get_pos.get_y(), 0);
+		}
+	}
+	al_flip_display();
+	
+}
+
 bool Grapher::set_background()
 {
 	background = al_load_bitmap(BACKGROUND_FILE);
@@ -34,6 +55,31 @@ bool Grapher::set_background()
 
 }
 
+bool Grapher::set_worm_images()
+{
+	std::string wFile = "wwalk-F";
+	std::string jFile = "wjump-F";
+	std::string png = ".png";
+	std::string currentPhoto;
+
+	for (int i = 1; i <= N_WALKING_IMAGES; i++)
+	{
+		currentPhoto =  wFile + std::to_string(i) + png;
+		walkingImages[i-1] = al_load_bitmap(currentPhoto.c_str());
+		if (!walkingImages[i - 1])
+			return false;
+	}
+	for (int j = 1; j <= N_JUMPING_IMAGES; j++)
+	{
+		currentPhoto = jFile + std::to_string(j) + png;
+		jumpingImages[j - 1] = al_load_bitmap(currentPhoto.c_str());
+		if (!jumpingImages[j - 1])
+			return false;
+	}
+
+	return true;
+}
+
 bool Grapher::init_allegro(int xDisplay, int yDisplay)
 {
 	if (!al_init())
@@ -44,7 +90,7 @@ bool Grapher::init_allegro(int xDisplay, int yDisplay)
 	if ((!display) || (!al_init_image_addon()))
 		return false;
 	
-	if (!setmode)
+	if ((!set_background()) || (!set_worm_images()))
 		return false;
 
 	return true;
