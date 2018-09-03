92f96e511b54451d3bcad5ad61d79f29f6e9f25b
Pablo Scheinfeld <scheinpablo@yahoo.com>
2018-09-02 20:14:51 -0700

    dfvsdb

--- TP_5/Grapher.cpp
+++ TP_5/Grapher.cpp
@@ -27,10 +27,11 @@ void Grapher::update(Worm * wArray[], int wormCount)
 {
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 	al_draw_bitmap(background, 0, 0, 0);
-	
+	ALLEGRO_BITMAP * bit;
 	for (int i = 0; i < wormCount; i++)
 	{
-		al_draw_bitmap(get_bitmap(wArray[i]->get_currentState(), wArray[i]->get_frameCounter()), wArray[i]->get_pos().get_x(), wArray[i]->get_pos().get_y(), (wArray[i]->get_orientation()) ? ALLEGRO_FLIP_HORIZONTAL : NO_ROTATION);
+		bit = get_bitmap(wArray[i]->get_currentState(), wArray[i]->get_frameCounter());
+		al_draw_bitmap(bit, wArray[i]->get_pos().get_x(), wArray[i]->get_pos().get_y(), (wArray[i]->get_orientation()) ? ALLEGRO_FLIP_HORIZONTAL : NO_ROTATION);
 	}
 	al_flip_display();
 	
@@ -83,6 +84,10 @@ ALLEGRO_BITMAP * Grapher::get_bitmap(wormState_n currentState, uint frameCounter
 			{
 				return jumpingImages[frameCounter - 5 - 1]; //El -1 es porque la foto 1 se encuentra en la posicion 0 del arreglo.
 			}
+			else
+			{
+				return walkingImages[0];
+			}
 			break;
 		case IDLE:
 			return walkingImages[0];
@@ -92,16 +97,44 @@ ALLEGRO_BITMAP * Grapher::get_bitmap(wormState_n currentState, uint frameCounter
 			{
 				return walkingImages[frameCounter - 2 - 1];
 			}
+			else 
+			{
+				return walkingImages[0];
+			}
 			break;
 		case MOVING:
 		case STOP_MOVING:
-			if ((frameCounter >= 1) && (frameCounter <= 8) || (frameCounter >= 1+14) && (frameCounter <= 8+14) || (frameCounter >= 1 + 14*2) && (frameCounter <= 8 + 14*2))
+			if ((frameCounter >= 1) && (frameCounter <= 8))
 			{
 				return walkingImages[frameCounter + 3 - 1];
 			}
-			else if ((frameCounter >= 9) && (frameCounter <= 13) || (frameCounter >= 9+14) && (frameCounter <= 13+14 || (frameCounter >= 9+14*2) && (frameCounter <= 13+14*2)))
+			else if ((frameCounter >= 1 + 14) && (frameCounter <= 8 + 14))
+			{
+				return walkingImages[frameCounter - 14 + 3 - 1];
+			}
+			else if ((frameCounter >= 1 + 14 * 2) && (frameCounter <= 8 + 14 * 2))
+			{
+				return walkingImages[frameCounter - 14*2 + 3 - 1];
+			}
+			else if ((frameCounter >= 1 + 14 * 3) && (frameCounter <= 8 + 14 * 3))
+			{
+				return walkingImages[frameCounter - 14 * 3 + 3 - 1];
+			}
+			else if ((frameCounter >= 9) && (frameCounter <= 13))
 			{
 				return walkingImages[frameCounter + 2 - 1];
+			}
+			else if ((frameCounter >= 9 + 14) && (frameCounter <= 13 + 14))
+			{
+				return walkingImages[frameCounter -14 + 2 - 1];
+			}
+			else if ((frameCounter >= 9 + 14 * 2) && (frameCounter <= 13 + 14 * 2))
+			{
+				return walkingImages[frameCounter - 14*2 + 2 - 1];
+			}
+			else if ((frameCounter >= 9 + 14 * 3) && (frameCounter <= 13 + 14 * 3))
+			{
+				return walkingImages[frameCounter - 14 * 3 + 2 - 1];
 			}
 			else if ((frameCounter % 14) == 0)
 			{
